ed3beba3ba774e2c4e5a57e018f70d66cb17c8ae
coder1110111 <ayush.yadav.dev610>
2025-01-28 10:14:02 +0530

    bodyParser Intro and how routes work

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware Function");
-    next();
-});
+app.use(bodyParser.urlencoded());
 
-app.use((req, res, next) => {
-    console.log("In another middleware");
-    res.send('<h1>Hello from Express!</h1>')
+app.use('/',(req, res, next) => {
+    console.log("This always runs!");
+    next();
 })
 
+app.get('/add-product',(req, res, next) => {
+    //console.log("In another middleware");
+    res.send('<form action="/product" method="POST"><input type="text" placeholder="title" name="title"><input type="text" placeholder="size" name="size"><button type="sumit">Add Product</button></form>')
+})
+
+app.post('/product', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req, res, next) => {
+    //console.log("In another middleware");
+    res.send('<h1>Hello from Express!</h1>')
+})
 
 
 app.listen(3000);
